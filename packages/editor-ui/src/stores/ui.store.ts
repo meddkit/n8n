ba5fdecf93268be02647598a6ab3049b9c004a95
lizailyina <lizailyina13052005@gmail.com>
2024-08-16 06:18:01 -0500

    change logo

--- packages/editor-ui/src/stores/ui.store.ts
+++ packages/editor-ui/src/stores/ui.store.ts
@@ -1,5 +1,5 @@
 import * as onboardingApi from '@/api/workflow-webhooks';
-import Logo from '@/assets/images/n8n-logo.png'
+// import Logo from '@/assets/images/n8n-logo.png'
 import {
 	ABOUT_MODAL_KEY,
 	CHAT_EMBED_MODAL_KEY,
@@ -625,7 +625,7 @@ export const useUIStore = defineStore(STORES.UI, () => {
 
 	return {
 		appliedTheme,
-		logo: Logo,
+		logo: '@/assets/images/n8n-logo.png',
 		contextBasedTranslationKeys,
 		getLastSelectedNode,
 		isVersionsOpen,
